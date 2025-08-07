3f391cf30473d46d010143ae46f632a27fc81297
Mr-MKZ <karimierfan74@gmail.com>
2025-08-07 16:03:50 +0330

    chore: get operation id from user

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,14 @@ use std::io::{self, Read, Write};
 
 fn main() {
     println!("Welcome to temperature converter");
+    println!("
+    1) Celcius to Fahrenheit
+    2) Fahrenheit to Celcius
+    ");
+    print!("Choose the option which you want: ");
+    let _ = io::stdout().flush();
+
+    let mut operation_mode = String::new();
+    let _ = io::stdin().read_line(&mut operation_mode);
+    let operation_mode = operation_mode.trim();
 }
